2f219142f4ba5f0ef08b6789aa661eb5745dcf19
pedrohlomba <pedrolomba27@gmail.com>
2024-04-04 21:55:42 -0300

    salvando

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, redirect, render_template, request, flash
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/guia_de_raca')
+def guia_de_raca():
+    return render_template('guia_de_raca.html')
+
+
+
+
+@app.route('/cadastrar', methods=['POST'])
+def cadastrar():
+    
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
+    email = request.form.get('email')
+    print(nome,senha,email)
+
+    return redirect('/guia_de_raca')
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
